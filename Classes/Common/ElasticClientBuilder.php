3480a220717b88622cb1caef8920392d3770b229
Matthias Richter <matthias.richter@slub-dresden.de>
2024-06-17 18:23:18 +0200

    Temporarily disable SSL connection to elasticsearch

--- Classes/Common/ElasticClientBuilder.php
+++ Classes/Common/ElasticClientBuilder.php
@@ -19,29 +19,29 @@ class ElasticClientBuilder extends ClientBuilder {
 
     public function autoconfig ()
     {
-		$extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('mpdb_core');
-		$hosts = [ $extConf['elasticHostName'] ];
-		$password = '';
+	$extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('mpdb_core');
+	$hosts = [ $extConf['elasticHostName'] ];
+	$password = '';
         $passwordFilePath = $this->getFileName($extConf['elasticPwdPath']);
-		if ($passwordFilePath != '') {
+	if ($passwordFilePath != '') {
             $passwordFile = fopen($passwordFilePath, 'r') or die($passwordFilePath . ' not found. Check your extension\'s configuration');
-			$password = trim(fgets($passwordFile));
-			fclose($passwordFile);
-		} else {
-			$hosts = [ 'http://' . $extConf['elasticHostName'] . ':9200' ];
-		}
-		$caFilePath = $this->getFileName($extConf['elasticCaFilePath']);
-
-		$this->sethosts($hosts);
-		if ($password) {
-			$this->setBasicAuthentication('elastic', $password);
-		}
-		if ($caFilePath) {
-			$this->setSSLVerification($caFilePath);
-		}
-
-		return $this;
+	    $password = trim(fgets($passwordFile));
+	    fclose($passwordFile);
+	} else {
+	    $hosts = [ 'http://' . $extConf['elasticHostName'] . ':9200' ];
 	}
+
+	$caFilePath = $this->getFileName($extConf['elasticCaFilePath']);
+	$this->sethosts($hosts);
+	if ($password) {
+	    $this->setBasicAuthentication('elastic', $password);
+	}
+	if ($caFilePath) {
+	    //$this->setSSLVerification($caFilePath);
+	    $this->setSSLVerification(false);
+	}
+	return $this;
+    }
 
     private function getFileName ($fileName)
     {
